9546d983cc8178e0c4830d9573b1dc23ab3425da
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-06 15:48:01 +0530

    Create program27.c

---
+++ C/program27.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{   
+    int iCnt = 0;
+    
+    // Updater
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for (iCnt=1; iCnt<=iNo ; iCnt++)
+    {
+        printf("Jay Ganesh!!!\n");
+    }
+}
+
+int main()
+{
+    int iCnt = 0;
+    int iValue = 0;
+
+    printf("Enter the Frequency: ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
