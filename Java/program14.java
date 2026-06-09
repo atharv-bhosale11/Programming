06399bf91028ec3e1297730d3f0c4adc68d19435
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-09 23:47:32 +0530

    add perfect number check program (Type 3)  - Implemented CheckPerfect method in NumberX class - Calculates sum of proper factors and compares with input

--- Java/program14.java
+++ Java/program14.java
@@ -6,7 +6,7 @@ class NumberX
     {
         // Logic
         int iCnt = 0;
-        int iSum = 0;
+        int iSum = 0; 
         for(iCnt = 1; iCnt <= (iNo/2); iCnt++)
         {
             if ((iNo % iCnt == 0))
