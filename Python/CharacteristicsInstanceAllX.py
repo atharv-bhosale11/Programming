be891dbebc47d6af26c3cdcc8414a02748fa62c5
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-11 19:35:05 +0530

    add Demo class showing class and instance variables with modifications

--- Python/CharacteristicsInstanceAllX.py
+++ Python/CharacteristicsInstanceAllX.py
@@ -4,8 +4,8 @@ class Demo:
     def __init__(self,A,B):
         self.Value1=A
         self.Value2=B
-
-print("Class Variable No:",Demo.No)
+ 
+print("Class Variable No:",Demo.No) 
 
 obj1=Demo(11,21)
 obj2=Demo(51,101)
