f50a86501a8ebcb9164fb44a17a5750b276138e3
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-12 10:07:34 +0530

    Enhance Python division program with structured exception handling. Handle ZeroDivisionError, ValueError, and generic exceptions gracefully. Demonstrate robust error management using try, except, and finally blocks.

--- Python/ExceptionDemo3.py
+++ Python/ExceptionDemo3.py
@@ -5,7 +5,7 @@ def main():
         print("Inside try") 
         print("Enter the first number: ")
         No1=int(input())
-
+ 
         print("Enter the Second number: ")
         No2=int(input())
         Ans = No1/No2
