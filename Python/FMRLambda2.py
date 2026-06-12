3feac4759db673e5c0e4aaa881a546e035edfa73
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-12 10:09:32 +0530

    Implement functional programming workflow using lambda expressions. Apply filter(), map(), and reduce() to process and aggregate list data efficiently. Demonstrate concise data transformation techniques with anonymous functions in Python.

--- Python/FMRLambda2.py
+++ Python/FMRLambda2.py
@@ -2,7 +2,7 @@ from functools import reduce
 CheckEven = lambda No: No%2==0
 Increment = lambda No : No+1
 Add = lambda A,B: A+B
-
+ 
 def main():
     Data = [11,10,15,20,22,27,30]
     print("Actual data is:  ",Data)
