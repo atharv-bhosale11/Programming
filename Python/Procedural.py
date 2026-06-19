ed8ea47d5c1987f4b4e281ba7f00eb6bf08cd2c8
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-19 22:54:56 +0530

    add Python program to perform addition and subtraction using standalone functions  - defined `Addition(A, B)` function to return sum of two numbers - defined `Substraction(A, B)` function to return difference of two numbers - declared variables `No1`, `No2`, and `Ans` for input and results - accepted user input for two integers using `input()` and converted to `int` - invoked `Addition(No1, No2)` and printed result - invoked `Substraction(No1, No2)` and printed result - reinforced understanding of function definition, function calls, and basic arithmetic operations in Python

---
+++ Python/Procedural.py
@@ -0,0 +1,18 @@
+def Addition(A,B):
+    return A+B
+
+def Substraction(A,B):
+    return A-B
+
+No1 = 0
+No2 = 0
+Ans = 0
+
+No1=int(input("Enter the First number: "))
+No2=int(input("Enter the Second number: "))
+
+Ans = Addition(No1,No2)
+print("Addition is: ",Ans)
+
+Ans = Substraction(No1,No2)
+print("Substraction is: ",Ans)
