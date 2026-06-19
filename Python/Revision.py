bf6f5cd6d57fd7c061a2a297352e57a3ac1f886c
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-19 22:56:38 +0530

    add Python program to demonstrate different data types including numeric, sequence, set, and dictionary  - printed integer value `no = 11` and its type `<class 'int'>` - printed float value `marks = 90.78` and its type `<class 'float'>` - printed complex number `x = 3+5j` and its type `<class 'complex'>` - demonstrated sequence types:   - list `arr = [10,20,30,40,10]` with type `<class 'list'>`   - tuple `brr = (10,20,30,40,10)` with type `<class 'tuple'>` - demonstrated remaining data types:   - set `arr = {10,20,30,40,10}` with type `<class 'set'>` (duplicates removed)   - dictionary `book = {"Name":"Let us see","Price":350,"Author":"Y Kanetkar"}` with type `<class 'dict'>` - accessed dictionary element `book["Price"]` to print value - reinforced understanding of Python’s core data types and their properties

---
+++ Python/Revision.py
@@ -0,0 +1,48 @@
+#Numeric DataTypes
+print("------------------------------------------------")
+no = 11
+print(no)
+print(type(no))
+
+print("------------------------------------------------")
+
+marks = 90.78
+print(marks)
+print(type(marks))
+
+print("------------------------------------------------")
+
+x = 3+5j
+print(x)
+print(type(x))
+
+print("------------------------------------------------")
+
+
+
+# Sequence 
+arr = [10,20,30,40,10]
+print(arr)
+print(type(arr))
+print("------------------------------------------------")
+
+brr = (10,20,30,40,10)
+print(brr)
+print(type(brr))
+print("------------------------------------------------")
+
+
+
+
+#Remaining All DataTypes
+
+arr = {10,20,30,40,10}
+print(arr)
+print(type(arr))
+print("------------------------------------------------") 
+
+book ={"Name" : "Let us see", "Price" : 350, "Author" : "Y Kanetkar"}
+print(book)
+print(type(book))
+print(book["Price"])
+print("------------------------------------------------")
