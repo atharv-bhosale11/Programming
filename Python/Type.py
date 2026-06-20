38e10163ae66f4461cae9810383b80b79ce4fce9
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-20 13:37:00 +0530

    add Python program to demonstrate different built-in data types and type checking  - declared integer variable `Age = 25` and printed its type `<class 'int'>` - declared float variable `Marks = 95.4` and printed its type `<class 'float'>` - declared string variable `City = "Pune"` and printed its type `<class 'str'>` - declared boolean variable `Food = False` and printed its type `<class 'bool'>` - declared complex number variable `Complex = 7+9j` and printed its type `<class 'complex'>` - used `type()` function to display the data type of each variable - reinforced understanding of Python’s core data types: int, float, str, bool, and complex

---
+++ Python/Type.py
@@ -0,0 +1,11 @@
+Age = 25    #int
+Marks = 95.4  #float
+City = "Pune"   #str
+Food = False    #boolean
+Complex = 7+9j  #complex
+
+print(type(Age))        #type is function
+print(type(Marks))
+print(type(City))
+print(type(Food))
+print(type(Complex))
