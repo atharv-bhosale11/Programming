3034c5d481e0860d79d79c0ef3ea81308200810e
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-01 20:44:28 +0530

    Add C program to demonstrate string termination behavior with multiple null characters   Defined `str` array containing characters with embedded `'\0'` to show how printing stops at the first null terminator.   Printed string using `%s` format specifier, resulting in output only up to the first `'\0'` character.

---
+++ C/program107.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[] = {'J','a','y','\0','G','a','n','e','s','h','\0'};        //'/0' will show character till /0
+
+    printf("%s\n",str);
+
+    return 0;
+}
