2c6f84d722da3ed594d686b12f71ecf1b10164a9
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-01 20:51:00 +0530

    Add Java program to demonstrate passing string to method in separate class   Created `StringX` class with `Display` method to print received string.   In `program113`, used `Scanner` to accept user input and passed string to `StringX.Display`.   Program shows how to handle string input and method invocation across classes.

---
+++ Java/program113.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class StringX
+{
+    public void Display(String str)
+    {
+        System.out.println("Received String is: "+str);
+    }
+}
+
+class program113
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        StringX strobj = new StringX();
+
+        String str = null;
+
+        System.out.println("Enter String: ");
+        str = sobj.nextLine();
+
+        strobj.Display(str);
+
+    }
+}
