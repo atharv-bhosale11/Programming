5eea985120ac7c7a5aeabc2568c7d83c375b8a84
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-11 17:42:57 +0530

    Add Java program to compare string length before and after trim

---
+++ Java/program140.java
@@ -0,0 +1,11 @@
+class program140
+{
+    public static void main(String A[]) 
+    {
+        String header = "    India  is  My  Country    ";
+        System.out.println("Before Trim: "+header.length());
+        header = header.trim();              
+
+        System.out.println("After Trim: "+header.length());
+    }
+}
