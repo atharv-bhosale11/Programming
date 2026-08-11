ac417ad782af0281dc6062fe4614b93edddd73e3
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-11 17:41:53 +0530

    Add Java program to trim string and print length

---
+++ Java/program138.java
@@ -0,0 +1,11 @@
+class program138
+{
+    public static void main(String A[]) 
+    {
+        String header = "    India is My Country    ";
+
+        header.trim();              // Issue
+
+        System.out.println(header.length());
+    }
+}
