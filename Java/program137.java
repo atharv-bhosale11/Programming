a793363cc58ffda9ecc56be41db720b5ffa6a59b
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-11 17:41:04 +0530

    Add Java program to calculate string length with spaces

---
+++ Java/program137.java
@@ -0,0 +1,9 @@
+class program137
+{
+    public static void main(String A[]) 
+    {
+        String header = "    India is My Country    ";
+
+        System.out.println(header.length());
+    }
+}
