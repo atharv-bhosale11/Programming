5bd2805eb472dfbe6886ff8862924502af53cacf
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-19 14:42:05 +0530

    Add Java program to count frequency of lowercase letters with filtered output   Implemented logic to trim and normalize spaces, converted string to lowercase, created frequency array of size 26, iterated through characters to update counts for 'a' to 'z', and displayed only letters with non-zero frequency values

---
+++ Java/program184.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class program184
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String: ");
+        String str = sobj.nextLine();
+
+        str = str.trim();
+
+        str = str.replaceAll("\\s+", " ");
+
+        str = str.toLowerCase();
+
+        char Arr[] = str.toCharArray();
+
+        int Frequency[] = new int[26];
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] >= 'a' && Arr[i] <= 'z')
+            {
+                Frequency[(int)Arr[i] - 97]++;
+            }
+        }
+        System.out.println("Frequency of Each letter is: ");
+
+        for(int i = 0 ; i < Frequency.length; i++)
+        {
+            if(Frequency[i] > 0)
+            {
+                System.out.println((char)(i + 97)+ " : "+Frequency[i]);
+            }
+        }   
+    }
+}
