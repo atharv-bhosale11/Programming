fee44edd622c79051065c9e8ce66a498fac23b31
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-21 09:10:25 +0530

    Add Java program to convert input string into character array and display each character   Implemented main method to accept string from user, converted string into character array using toCharArray, iterated through array with loop, and printed each character sequentially

---
+++ Java/program201.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class program201
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+       System.out.println("Enter String : ");
+       String str = sobj.nextLine();
+
+       char Arr[] = str.toCharArray();
+
+       for(int i = 0; i < Arr.length; i++)
+       {
+            System.out.println(Arr[i]);
+       }
+    }
+}
