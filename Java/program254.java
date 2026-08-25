34734a4f36d498af2b5460917c3f852459588b20
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-25 19:21:06 +0530

    Add Java program to demonstrate modulus operator with different divisors   Implemented program to define a fixed integer and applied modulus operation with 10, 100, and 1000 to extract last digit, last two digits, and last three digits respectively. Printed results to show how modulus operator can be used to isolate portions of a number.

---
+++ Java/program254.java
@@ -0,0 +1,11 @@
+class program254
+{
+    public static void main(String A[])
+    {
+        int iNo = 141376;
+
+        System.out.println(iNo % 10);
+        System.out.println(iNo % 100);
+        System.out.println(iNo % 1000);
+    }
+}
