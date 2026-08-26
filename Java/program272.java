05422a1ef91ed8e548c45d28d9a7dbfb9b70f041
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-26 18:31:40 +0530

    Add Java program to demonstrate use of final class in inheritance hierarchy   Defined GetterSetter interface with Accept and Display methods. Implemented ArrayX class with protected members, dynamic allocation via constructor, input of elements using Scanner in Accept method, and output of elements with tab separation in Display method. Declared Searching class as final, preventing further inheritance. Attempted to extend Searching in Demo class, which results in compilation error since final classes cannot be subclassed. In main method, created Searching object of size 5, invoked Accept to read values, and Display to show stored elements, illustrating restriction of inheritance using final keyword.

---
+++ Java/program272.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+interface GetterSetter
+{
+    void Accept();
+    void Display();
+}
+
+class ArrayX implements GetterSetter
+{
+    protected  int Arr[];
+    protected  int iSize;
+
+    public ArrayX(int iSize)
+    {
+        this.iSize = iSize;
+        Arr = new int[iSize];
+    }
+
+    public void Accept()
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the Number: ");
+
+        for(int i = 0; i < iSize; i++)
+        {
+            Arr[i] = sobj.nextInt();
+        }
+    }
+
+    public void Display()
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Elements of the Array are: ");
+
+        for(int i = 0; i < iSize; i++)
+        {
+            System.out.print(Arr[i]+"\t");
+        }
+        System.out.println();
+    }
+}
+
+final class Searching extends ArrayX
+{
+    public Searching(int iSize)
+    {
+        super(iSize);
+    }
+}
+
+class Demo extends Searching        // Error 
+{
+
+}
+
+class program272
+{
+    public static void main(String A[])
+    {
+        Searching sobj = new Searching(5);
+
+        sobj.Accept();
+        sobj.Display();
+    }
+}
