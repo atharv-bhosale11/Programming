d9a2b4e1907dc1d5cae566a9e91ac61d99b57035
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-31 20:15:51 +0530

    Add Java program to implement singly linked list with fast and slow pointer middle element search   Defined node class with data and next pointer, and SinglyLL class with private first pointer and node count. Added InsertFirst and InsertLast methods to insert nodes, Display method to traverse and print nodes, and Count method to return total number of nodes. Implemented MiddleElement using count-based traversal and MiddleElementX using fast and slow pointer technique to efficiently find the middle node. In program306 main method, instantiated SinglyLL object, inserted nodes, displayed the list, and printed the middle element using MiddleElementX.

---
+++ Java/program306.java
@@ -0,0 +1,122 @@
+class node
+{
+    public int data;
+    public node next;
+
+    public node(int no)
+    {
+        this.data = no;
+        this.next = null;
+    }
+}
+
+class SinglyLL
+{
+    private node first;
+    private int iCount;
+
+    public SinglyLL()
+    {
+        this.first = null;
+        this.iCount = 0;
+    }
+
+    public void InsertFirst(int iNo)
+    {
+        node newn = new node(iNo);
+
+        newn.next = first;
+        first = newn;
+        
+        iCount++;
+    }
+
+    public int Count()
+    {
+        return iCount;
+    }
+
+    public void Display()
+    {
+        node temp = null;
+        temp = first;
+
+        while(temp != null)
+        {
+            System.out.print("| " +temp.data+" | ->  ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public void InsertLast(int iNo)
+    {
+        node newn = new node(iNo);
+
+        if(first == null)
+        {
+            first = newn;
+        }
+        else
+        {
+            node temp = first;
+
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = newn;
+        }
+        iCount++;
+    }
+
+    // Time Complexity : N + N / 2
+    
+    public int MiddleElement()
+    {
+        int iCount = Count();
+
+        int iMiddle = iCount / 2;
+        node temp = first;
+        for(int i = 1; i <= iMiddle; i++)
+        {
+            temp = temp.next;
+        }
+        return temp.data;
+    }
+
+    public int MiddleElementX()
+    {
+        node fast = first;
+        node slow = first;
+
+        while(fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow.data;
+    }
+}   
+
+class program306
+{
+    public static void main(String A[])
+    {
+        int iRet;
+        SinglyLL sobj = new SinglyLL();
+
+        sobj.InsertFirst(51);
+        sobj.InsertFirst(21);
+        sobj.InsertFirst(11);
+        sobj.InsertLast(101);
+        sobj.InsertLast(111);
+
+        sobj.Display();
+
+        iRet = sobj.MiddleElementX();
+        System.out.println("Middle Element is   :"+iRet);
+
+        
+    }
+}
