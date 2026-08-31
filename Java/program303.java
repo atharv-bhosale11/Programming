9f5ba97b4853600652b3b845b4a0552b710cf42f
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-31 20:03:30 +0530

    Add Java program to implement singly linked list with InsertFirst, InsertLast, Display, and Count operations   Defined node class with data and next pointer, and SinglyLL class with private first pointer and node count. Added InsertFirst method to add new node at the beginning, InsertLast method to add new node at the end, Display method to traverse and print all nodes, and Count method to return total number of nodes. In program303 main method, instantiated SinglyLL object, inserted nodes using both InsertFirst and InsertLast, displayed the list, and printed the count of nodes.

---
+++ Java/program303.java
@@ -0,0 +1,93 @@
+class node
+{
+    public int data;
+    public node next;
+
+    public node(int no)
+    {
+        this.data = no;
+        this.next = null;
+    }
+}
+
+class SinglyLL
+{
+    private node first;
+    private int iCount;
+
+    public SinglyLL()
+    {
+        this.first = null;
+        this.iCount = 0;
+    }
+
+    public void InsertFirst(int iNo)
+    {
+        node newn = new node(iNo);
+
+        newn.next = first;
+        first = newn;
+        
+        iCount++;
+    }
+
+    public int Count()
+    {
+        return iCount;
+    }
+
+    public void Display()
+    {
+        node temp = null;
+        temp = first;
+
+        while(temp != null)
+        {
+            System.out.print("| " +temp.data+" | ->");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public void InsertLast(int iNo)
+    {
+        node newn = new node(iNo);
+
+        if(first == null)
+        {
+            first = newn;
+        }
+        else
+        {
+            node temp = first;
+
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = newn;
+        }
+        iCount++;
+    }
+}   
+
+class program303
+{
+    public static void main(String A[])
+    {
+        int iRet;
+        SinglyLL sobj = new SinglyLL();
+
+        sobj.InsertFirst(51);
+        sobj.InsertFirst(21);
+        sobj.InsertFirst(11);
+        sobj.InsertLast(101);
+        sobj.InsertLast(111);
+
+        sobj.Display();
+
+        iRet = sobj.Count();
+
+        System.out.println("Number of Nodes     : "+iRet);
+    }
+}
