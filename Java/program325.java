bcfa943a6f594909c2231dc3a93e778fa69e7362
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-05 13:57:24 +0530

    Add Java program to print all command-line arguments   Defined class program325 with main method. Iterated through command-line arguments array A using for loop, printed each argument sequentially with System.out.println. In program325 main method, executed logic to display all arguments passed during program execution.

---
+++ Java/program325.java
@@ -0,0 +1,10 @@
+class program325
+{
+    public static void main(String A[])
+    {
+        for(int i = 0; i < A.length; i++)
+        {
+            System.out.println(A[i]);
+        }
+    }
+}
