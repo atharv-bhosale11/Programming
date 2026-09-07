a88cf2441a65e2f5437c50a0119bbb267a24df59
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-07 16:49:46 +0530

    Add Java program to sort list of strings using Collections.sort   Defined class program342 with main method. Created ArrayList of strings containing names. Printed original list showing insertion order. Applied Collections.sort to sort names alphabetically. Printed sorted list to display names in ascending lexicographical order.

---
+++ Java/program342.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class program342
+{
+    public static void main(String A[])
+    {
+        ArrayList <String> aobj = new ArrayList<String>();
+
+        aobj.add("Rahul");
+        aobj.add("Amit");
+        aobj.add("Pooja");
+        aobj.add("Neha");
+        aobj.add("Kiran");
+
+        System.out.println(aobj);
+
+        Collections.sort(aobj);
+
+        System.out.println(aobj);
+    }
+}
