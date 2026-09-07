864c61b11fb65cc9ca3d5529fd8467b7f5686298
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-07 16:47:51 +0530

    Add Java program to identify duplicate transactions using HashSet with repeated entries   Defined class program339 with main method. Created string array containing transaction IDs including multiple occurrences of TX101 and TX102. Used HashSet unique to store non-repeated transactions and another HashSet duplicate to track repeated ones. Introduced boolean variable bRet to capture result of add operation. Iterated through transaction array, added IDs to unique set, and when add returned false, marked transaction as duplicate. Printed duplicate transactions separately by iterating through duplicate set, displaying each duplicate transaction ID once.

---
+++ Java/program339.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class program339
+{
+    public static void main(String A[])
+    {
+        String transaction[] = {"TX101","TX102","TX103","TX101","TX104","TX102","TX101"};
+
+        HashSet <String> unique    = new HashSet<String>();
+        HashSet <String> duplicate = new HashSet<String>();
+
+        boolean bRet = false;
+
+        for(String str : transaction)
+        {
+            
+            bRet = unique.add(str);
+
+            if(bRet == false)
+            {
+                duplicate.add(str);
+            }
+            
+        }
+
+        System.out.println("Duplicate Transactions are: ");
+        for(String str : duplicate)
+        {
+            System.out.println(str);
+        }
+    }
+}
