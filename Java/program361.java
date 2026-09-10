cf4780bc3613242750e3da12a294c3a173b63b94
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-10 16:29:31 +0530

    Add Java program to demonstrate String split method for counting words   Defined class program361 with main method. Created string variable header initialized with "India is my country". Applied split method using space delimiter to divide string into words and stored them in array Arr. Printed number of words by displaying length of array, showing how split can be used to count words in a sentence.

--- Java/program361.java
+++ Java/program361.java
@@ -3,7 +3,7 @@ class program361
     public static void main(String A[])
     {
         String header = "India is my country";
-
+ 
         String Arr[] = header.split(" ");
  
         System.out.println("Number of words are : "+Arr.length);
