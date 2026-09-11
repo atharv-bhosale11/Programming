f4ab895127b3d5aeac871d5c795599ad631604a1
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-11 09:35:44 +0530

    Add C program to open a file in read-write append mode, write complete data string, and display number of bytes written   Included stdio.h, fcntl.h, unistd.h, and string.h headers for file operations. Defined main function with integer file descriptor and return value variables. Declared character array data initialized with "Marvellous Infosystems". Invoked open system call to open file "ABC.txt" in read-write mode with O_APPEND flag to ensure new data is always added at the end of the file. Checked return value to handle error case and printed appropriate message if file opening fails. Printed success message along with file descriptor value when file is opened successfully. Used write system call to append entire string data into the file by passing strlen(data) as size, and stored number of bytes written in iRet. Printed number of bytes successfully written. Closed the file using close system call to release resources. Program demonstrates file opening in append mode, writing complete string, and reporting bytes written using low-level system calls in C.

---
+++ C/program261.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<fcntl.h>      // Only Linux based 
+#include<unistd.h> 
+#include<string.h> 
+
+int main()
+{
+    int fd = 0;             // File Descriptior
+    int iRet = 0;
+    char data[] = "Marvellous Infosystems";
+    fd = open("ABC.txt",O_RDWR | O_APPEND);
+
+    if(fd == -1)
+    {
+        printf("Unable to Open File\n");
+    }
+    else
+    {
+        printf("File Gets Succesfully Open with fd : %d\n",fd);
+
+        iRet = write(fd,data,strlen(data));
+
+        printf("%d bytes get succesfuuly return\n",iRet);   
+
+        close(fd);
+    }
+    return 0;
+}
