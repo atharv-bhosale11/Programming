d1bfd48d51c342d3fe56f311e71a655e4e51be08
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-17 15:16:34 +0530

    Add StudyTracker with ArrayList field   Implemented StudyTracker class containing an ArrayList of StudyLog objects and instantiated StudyTracker in main method for demonstration.

---
+++ Java/program398.java
@@ -0,0 +1,59 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+class StudyLog
+{
+    private LocalDate Date;
+    private String Subject;
+    private double Duration;
+    private String Description;
+
+    public StudyLog(LocalDate a, String b, double c, String d)
+    {
+        this.Date = a;
+        this.Subject = b;
+        this.Duration = c;
+        this.Description = d;
+    }
+
+    @Override
+    public String toString()
+    {
+        return Date + " | " + Subject + " | " + Duration + " | " + Description;
+    }
+
+    public LocalDate getDate()
+    {
+        return this.Date;
+    }
+
+    public String getSubject()
+    {
+        return this.Subject;
+    }
+
+    public double getDuration()
+    {
+        return this.Duration;
+    }
+
+    public String getDescription()
+    {
+        return this.Description;
+    }
+}
+
+class StudyTracker
+{
+    public ArrayList<StudyLog> Database = new   ArrayList<StudyLog>();
+
+}
+
+class program398
+{
+    public static void main(String A[]) throws Exception
+    {
+        StudyTracker stobj = new StudyTracker();
+        
+    }
+}
