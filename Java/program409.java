7e24218958f2188422075a4a1502e65179fdf145
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-18 10:55:56 +0530

    Add TreeMap navigation methods demo   Implemented TreeMap with integer keys and string values, demonstrated retrieval of firstKey, lastKey, firstEntry, higherKey, and lowerKey to showcase sorted map navigation features.

--- Java/program409.java
+++ Java/program409.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class program409
 {
-    public static void main(String A[])
+    public static void main(String A[]) 
     {
         TreeMap <Integer,String> hobj = new TreeMap<Integer,String>();
 
