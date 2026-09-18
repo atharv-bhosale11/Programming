030914367dec7a3172db43d5bc424b86971c869c
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-18 10:49:05 +0530

    Add HashMap duplicate key demo   Implemented HashMap with integer keys and string values, demonstrated overwriting behavior when inserting duplicate keys, and displayed final map contents.

--- Java/program405.java
+++ Java/program405.java
@@ -5,7 +5,7 @@ class program405
     public static void main(String A[])
     {
         HashMap<Integer,String> hobj = new HashMap<Integer,String>();
-
+ 
         hobj.put(20,"C Programming");
         hobj.put(10,"Java Programming");
         hobj.put(30,"C Programming");
