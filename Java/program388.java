e199203b0af259b123b1edfb95f22b4487f9a897
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-20 11:26:06 +0530

    Implemented encapsulation by making data members private and providing getter methods for controlled access.

--- Java/program388.java
+++ Java/program388.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 class StudyLog
 {
-    private LocalDate Date;
+    private LocalDate Date; 
     private String Subject;
     private double Duration;
     private String Description;
