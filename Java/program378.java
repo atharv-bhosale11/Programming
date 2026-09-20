cea25dfd17818c1ae928da7ea9d39b76fc86f83f
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-20 11:15:48 +0530

    Implemented file packing utility with fixed-size header generation for folder files

--- Java/program378.java
+++ Java/program378.java
@@ -6,7 +6,7 @@ class program378
     public static void main(String A[]) throws Exception
     {     
         int iRet = 0;
-        int Size = 0;
+        int Size = 0; 
         int i = 0, j = 0;
 
         Scanner sobj = new Scanner(System.in);
