7a2d8be18fe1149c02ce85287de067ad56e63a21
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-22 22:22:16 +0530

    Added ArrayList copy operation using Collections framework

--- Java/program434.java
+++ Java/program434.java
@@ -5,7 +5,7 @@ class program434
     public static void main(String A[])
     { 
       
-        ArrayList<Integer> src = new ArrayList<Integer>();
+        ArrayList<Integer> src = new ArrayList<Integer>(); 
 
         src.add(10);
         src.add(20);
