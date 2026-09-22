a61a6c74ea545a6a1834399dd7e555eae27f28dd
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-22 19:28:13 +0530

    Implemented element frequency counting in collection

--- Java/program433.java
+++ Java/program433.java
@@ -4,7 +4,7 @@ class program433
 {
     public static void main(String A[])
     {
-        ArrayList<Integer> list = new ArrayList<Integer>(); 
+        ArrayList<Integer> list = new ArrayList<Integer>();  
  
         list.add(10);
         list.add(20);
