ddd5129b65bb60c333948b129577944e83f9bce7
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-22 22:15:33 +0530

    Implemented element swapping within ArrayList

--- Java/program435.java
+++ Java/program435.java
@@ -5,7 +5,7 @@ class program435
     public static void main(String A[])
     { 
         ArrayList<Integer> list = new ArrayList<Integer>();
-
+ 
         list.add(10);
         list.add(20);
         list.add(30); 
