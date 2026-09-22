a368ef1ab75694e16e37c6c225f19de3efa59462
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-22 22:52:44 +0530

    Implemented conditional element removal from collection

---
+++ Java/program441.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class program441
+{
+    public static void main(String A[])
+    {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(10);
+        list.add(15);
+        list.add(20);
+        list.add(25);
+
+        list.removeIf(n -> n % 2 == 0);
+
+        System.out.println(list);
+    }
+}
