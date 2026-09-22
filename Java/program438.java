7c720815d6b4d2c263d2d90f7d1b54a396723211
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-22 22:35:48 +0530

    Added collection intersection functionality

--- Java/program438.java
+++ Java/program438.java
@@ -4,11 +4,9 @@ class program438
 {
     public static void main(String A[])
     {
-        ArrayList<Integer> A1 =
-            new ArrayList<Integer>();
+        ArrayList<Integer> A1 = new ArrayList<Integer>();
 
-        ArrayList<Integer> A2 =
-            new ArrayList<Integer>();
+        ArrayList<Integer> A2 = new ArrayList<Integer>();
 
         A1.add(10);
         A1.add(20);
