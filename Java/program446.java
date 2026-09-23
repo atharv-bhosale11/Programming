b31b45f629be122061dfc1206c9f166c8848ca34
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-23 16:45:52 +0530

    Implemented sorted key value storage using TreeMap

---
+++ Java/program446.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class program446
+{
+    public static void main(String A[])
+    {
+        TreeMap<Integer,String> tobj =
+            new TreeMap<Integer,String>();
+
+        tobj.put(3,"Java");
+        tobj.put(1,"C");
+        tobj.put(2,"C++");
+
+        System.out.println(tobj);
+    }
+}
