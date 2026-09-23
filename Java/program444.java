13434243fbdc11daab49dc2ee85d4dd61a20f160
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-23 16:44:38 +0530

    Implemented automatic sorting using TreeSet collection

---
+++ Java/program444.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class program444
+{
+    public static void main(String A[])
+    {
+        TreeSet<Integer> tobj = new TreeSet<Integer>();
+
+        tobj.add(50);
+        tobj.add(10);
+        tobj.add(30);
+        tobj.add(20);
+
+        System.out.println(tobj);
+    }
+}
