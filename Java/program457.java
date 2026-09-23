acb4a0b05fe9c0d45bcae9f71f6559eca90f0b37
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-23 17:00:10 +0530

    Added ordered duplicate removal using LinkedHashSet

--- Java/program457.java
+++ Java/program457.java
@@ -4,8 +4,7 @@ class program457
 {
     public static void main(String A[])
     {
-        ArrayList<Integer> list =
-            new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
 
         list.add(10);
         list.add(20);
