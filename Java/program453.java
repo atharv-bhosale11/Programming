5b2c9aa2b04234fb903ac87eb16129d554295b60
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-23 16:51:15 +0530

    Added double ended queue implementation using ArrayDeque

---
+++ Java/program453.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class program453
+{
+    public static void main(String A[])
+    {
+        ArrayDeque<Integer> dobj =
+            new ArrayDeque<Integer>();
+
+        dobj.addFirst(10);
+        dobj.addLast(20);
+
+        System.out.println(dobj);
+    }
+}
