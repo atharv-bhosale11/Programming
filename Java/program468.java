8727fffbfb0a0d6dc3aa4915f303ce1d8c4dfb6c
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-24 19:34:26 +0530

    Implemented duplicate removal preserving insertion order

---
+++ Java/program468.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class program468
+{
+    public static void main(String A[])
+    {
+        ArrayList<String> names =
+            new ArrayList<String>();
+
+        names.add("Atharv");
+        names.add("Rahul");
+        names.add("Atharv");
+        names.add("Amit");
+        names.add("Rahul");
+
+        LinkedHashSet<String> unique =
+            new LinkedHashSet<String>(names);
+
+        System.out.println(unique);
+    }
+}
