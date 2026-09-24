639762d5b25fcc386e08dc6a593c84709eb266af
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-24 13:11:33 +0530

    Implemented common element finder using collections

---
+++ Java/program467.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class program467
+{
+    public static void main(String A[])
+    {
+        ArrayList<Integer> list1 =
+            new ArrayList<Integer>(Arrays.asList(10,20,30,40,50));
+
+        ArrayList<Integer> list2 =
+            new ArrayList<Integer>(Arrays.asList(30,40,50,60,70));
+
+        ArrayList<Integer> common =
+            new ArrayList<Integer>(list1);
+
+        common.retainAll(list2);
+
+        System.out.println("Common Elements : " + common);
+    }
+}
