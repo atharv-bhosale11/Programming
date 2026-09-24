6d009a83b89650fe36abcd8c8b8180ed52daa40d
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-24 12:55:37 +0530

    Implemented student grouping by grade using HashMap

---
+++ Java/program461.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class program461
+{
+    public static void main(String A[])
+    {
+        HashMap<String,ArrayList<String>> hmap =
+            new HashMap<String,ArrayList<String>>();
+
+        hmap.put("A",new ArrayList<String>());
+        hmap.put("B",new ArrayList<String>());
+        hmap.put("C",new ArrayList<String>());
+
+        hmap.get("A").add("Atharv");
+        hmap.get("A").add("Rahul");
+
+        hmap.get("B").add("Amit");
+        hmap.get("B").add("Rohit");
+
+        hmap.get("C").add("Sagar");
+
+        for(Map.Entry<String,ArrayList<String>> entry : hmap.entrySet())
+        {
+            System.out.println("Grade : " + entry.getKey());
+            System.out.println("Students : " + entry.getValue());
+        }
+    }
+}
