89accf9a4ac9ea3d7b033f78fb9567a5161e5590
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-24 12:50:48 +0530

    Implemented student management system using HashMap

---
+++ Java/program459.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class program459
+{
+    public static void main(String A[])
+    {
+        HashMap<Integer,String> students = new HashMap<>();
+
+        students.put(101,"Atharv");
+        students.put(102,"Rahul");
+        students.put(103,"Amit");
+
+        System.out.println("Students : "+students);
+
+        System.out.println("Search Roll 102 : "
+                            + students.get(102));
+
+        students.put(102,"Rohit");
+
+        students.remove(103);
+
+        System.out.println("Updated Data : "
+                            + students);
+    }
+}
