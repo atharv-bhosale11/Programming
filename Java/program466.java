c154ddfab747e0f03a79f265ece8592112c73b96
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-24 13:10:06 +0530

    Implemented inventory management system using HashMap

---
+++ Java/program466.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class program466
+{
+    public static void main(String A[])
+    {
+        HashMap<String,Integer> inventory =
+            new HashMap<String,Integer>();
+
+        inventory.put("Laptop",10);
+        inventory.put("Mouse",25);
+        inventory.put("Keyboard",15);
+        inventory.put("Monitor",8);
+
+        System.out.println("Inventory Details");
+
+        for(Map.Entry<String,Integer> entry : inventory.entrySet())
+        {
+            System.out.println(entry.getKey()+" : "
+                                + entry.getValue());
+        }
+    }
+}
