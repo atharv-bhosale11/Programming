e1f9aeeed9c003bb9c39b1e81cd0cb5f955491f3
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-25 07:39:07 +0530

    Create program472.java

---
+++ Java/program472.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class program472
+{
+    public static void main(String A[])
+    {
+        String votes[] =
+        {
+            "A","B","A","C","A",
+            "B","C","B","A"
+        };
+
+        HashMap<String,Integer> result =
+            new HashMap<String,Integer>();
+
+        for(String vote : votes)
+        {
+            result.put(vote,
+                result.getOrDefault(vote,0)+1);
+        }
+
+        System.out.println(result);
+    }
+}
