a47ea8006e2f29894ce6cdea1382aa2c614c2666
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-25 07:40:13 +0530

    Implemented vote counting system using HashMap

--- Java/program472.java
+++ Java/program472.java
@@ -14,7 +14,7 @@ class program472
         for(String vote : votes)
         {
             result.put(vote,result.getOrDefault(vote,0)+1);
-        }
+        } 
 
         System.out.println(result);
     }
