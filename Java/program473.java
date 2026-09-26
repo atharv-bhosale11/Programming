d6cf919f3f8fe82e5ba373df92ce3a6904e3be76
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-09-26 12:51:01 +0530

    Implemented attendance tracking using collections

---
+++ Java/program473.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class program473
+{
+    public static void main(String A[])
+    {
+        HashMap<String,Boolean> attendance =
+            new HashMap<String,Boolean>();
+
+        attendance.put("Sujit",true);
+        attendance.put("Rajan",false);
+        attendance.put("Amit",true);
+        attendance.put("Rohit",false);
+
+        System.out.println("Absent Students");
+
+        for(Map.Entry<String,Boolean> entry :
+            attendance.entrySet())
+        {
+            if(entry.getValue() == false)
+            {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+}
